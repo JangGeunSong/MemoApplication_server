977291ab62b614ab8d16d021a6541d4f8f722f3c
JangGeunSong <sjg1468@gmail.com>
2020-02-02 22:48:48 +0900

    테스트코드 라우터별 추가

--- __test__/server.test.ts
+++ __test__/server.test.ts
@@ -1,8 +1,44 @@
 import request from 'supertest'
+import http from 'http'
 import app from '../server'
 
 describe('tset to server data get successfully', () => {
+
+    let server: http.Server;
+
+    beforeAll((done) => {
+      server = http.createServer(app);
+      server.listen(done);
+    });
+  
+    afterAll((done) => {
+      server.close(done);
+    });
+
     test('should get data', () => {
-        request(app).get('/memos').expect(200)
+        request(app)
+            .get('/memos')
+            .expect(200)
+    })
+
+    test('should post data', () => {
+        request(app)
+            .post('/memo')
+            .send({ title: 'hello', description: 'hi', color: '#fff' })
+            .expect(200)
+    })
+    
+    test('should delete data', () => {
+        request(app)
+            .delete('/memo')
+            .send({ title: 'title1' })
+            .expect(200)
+    })
+    
+    test('should put data', () => {
+        request(app)
+            .put('/memo')
+            .send({ title: 'title1', description: 'hi', color: '#fff' })
+            .expect(200)
     })
 })
